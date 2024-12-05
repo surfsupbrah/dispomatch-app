560a19f4a9142241c41f1006ca4d15f68f5e2017
surfsupbrah <martinjcotti@gmail.com>
2024-12-05 13:04:11 -0500

    updates

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,11 +5,11 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl) {
-  throw new Error('https://vrffzozygukacmussykj.supabase.co');
+  throw new Error('Missing VITE_SUPABASE_URL environment variable');
 }
 
 if (!supabaseKey) {
-  throw new Error('Missing VITE_SUPABASE_ANON_KeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyZmZ6b3p5Z3VrYWNtdXNzeWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM0MTk1ODUsImV4cCI6MjA0ODk5NTU4NX0.3YJoeKiXeyulTItwVKsj5yHif69tRF_iJIDN_qU7vswEY environment variable');
+  throw new Error('Missing VITE_SUPABASE_ANON_KEY environment variable');
 }
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
