296c7df61dcfb1ea094b808a06930b532e469242
surfsupbrah <martinjcotti@gmail.com>
2024-12-05 13:58:50 -0500

    email auth v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import { Header } from './components/Header';
-import { HomePage } from './pages/HomePage';
-import { LoginPage } from './pages/LoginPage';
-import { SignupPage } from './pages/SignupPage';
-import { FacilityPage } from './pages/FacilityPage';
-import { FacilityDashboard } from './pages/FacilityDashboard';
-import { SearchResultsPage } from './pages/SearchResultsPage';
-import { ProtectedDashboard } from './components/ProtectedDashboard';
+import { AppRoutes } from './routes';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-50">
-          <Header />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/search" element={<SearchResultsPage />} />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedDashboard>
-                  <FacilityDashboard />
-                </ProtectedDashboard>
-              }
-            />
-            <Route
-              path="/facility/:id"
-              element={
-                <ProtectedDashboard>
-                  <FacilityPage />
-                </ProtectedDashboard>
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </Router>
   );
 }
---
+++ src/routes.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
+import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LoginPage';
+import { SignupPage } from './pages/SignupPage';
+import { FacilityPage } from './pages/FacilityPage';
+import { FacilityDashboard } from './pages/FacilityDashboard';
+import { SearchResultsPage } from './pages/SearchResultsPage';
+import { ProtectedDashboard } from './components/ProtectedDashboard';
+
+export function AppRoutes() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/search" element={<SearchResultsPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedDashboard>
+              <FacilityDashboard />
+            </ProtectedDashboard>
+          }
+        />
+        <Route
+          path="/facility/:id"
+          element={
+            <ProtectedDashboard>
+              <FacilityPage />
+            </ProtectedDashboard>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
