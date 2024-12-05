b2090be01707c37cfaf6add142227e284cad3c7a
surfsupbrah <martinjcotti@gmail.com>
2024-12-05 14:06:59 -0500

    email auth v5

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import { AppRoutes } from './routes';
+import { AppRoutes } from './routes.tsx';
 
 export default function App() {
   return (
